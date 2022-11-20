7e0001e4ed3b3f272e4caaf59bbfd7be504cd1c7
kiwi_j500 <mingureion@gmail.com>
2022-11-21 00:51:27 +0900

    a

--- problems/boj2156_포도주.py
+++ problems/boj2156_포도주.py
@@ -1,13 +1,20 @@
 # n = int(input())
 # l = [int(input()) for _ in range(n)]
 n=6
-l = [6,10,13,9,8,1]
-ar = sorted(l)
-p = [0 for _ in range(n)]
+l = [6,10,13,9,8,1] #33
+# l = [6,10,13]
+if n<3:    print(sum(l))
 
-print(p)
-
-for i in range(n):
-    ar[i]
+else:
+    res = 0    
+    t = [0, l[0],l[0]+l[1]] + [0 for _ in range(n-2)]  # t[i] = i잔 까지 고려한  최대값
+     
+    for i in range(2,n):
+        print('I: ',i,'   l[i]',l[i])
+        t[i+1] = max(
+            t[i],                     # 이번 잔은 안 마심          16
+            l[i] + t[i-1] + l[i-1],     # 이번 잔과 이전 잔을 마심 
+            l[i] + t[i-2] + l[i-2]      # 이번 잔과 전전 잔을 마심 0 6 13
+        )
+    print(t[n])
     
-#발로 짰는데 시간초과는 안걸림, DP로 수정예정..
--- python_st/test.py
+++ python_st/test.py
@@ -1,8 +1,16 @@
+import time
+start = time.time()  # 시작 시간 저장
 from sys import stdin
 
 
+l = [6,10,13,9,8,1]
+n = 6
+t = [0, l[0],l[0]+l[1]]+ [0 for _ in range(n-3)]  # t[i] = i잔 까지 고려한  최대값, 
 
-a = input().rstrip()
+print(t)
 
 
-print(a)
+        
+
+ 
+print("실행시간 :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
