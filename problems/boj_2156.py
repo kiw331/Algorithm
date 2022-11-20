98080d3dc4b2e4ee35b29365b56ccd003940c496
kiwi_j500 <mingureion@gmail.com>
2022-11-20 13:17:09 +0900

    up

---
+++ problems/boj_2156.py
@@ -0,0 +1,13 @@
+# n = int(input())
+# l = [int(input()) for _ in range(n)]
+n=6
+l = [6,10,13,9,8,1]
+ar = sorted(l)
+p = [0 for _ in range(n)]
+
+print(p)
+
+for i in range(n):
+    ar[i]
+    
+#발로 짰는데 시간초과는 안걸림, DP로 수정예정..
---
+++ python_st/test.py
@@ -0,0 +1,8 @@
+from sys import stdin
+
+
+
+a = input().rstrip()
+
+
+print(a)
