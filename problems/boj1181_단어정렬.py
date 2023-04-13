bb036f14d6a3ab11a75df8ffa1e30a35d5f74c73
kiwi_j500 <mingureion@gmail.com>
2023-04-14 03:54:37 +0900

    class2 --

---
+++ problems/boj1181_단어정렬.py
@@ -0,0 +1,8 @@
+n = int(input())
+l = list(set([ input() for _ in range(n)])) # 중복단어제거, (정렬함수 쓰려고)다시 리스트로 바꿔줌
+
+l.sort(key=lambda x : (len(x),x))   # # 길이, 사전순으로 정렬, 키에 변수가 그대로 들어가면 사전 순
+
+print(*l, sep='\n')
+
+
--- problems/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-for i in range(t):
-    h, w, n = map(int,input().split())   # h, w, n
-    a, b = math.ceil(n/h), n % h    #몫 -> ~호, 나머지-> 층 / 0~h까지는 1호니까 천장함수 사용
-    print(str(b) +str(a).zfill(2))
