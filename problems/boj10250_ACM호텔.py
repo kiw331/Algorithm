b118d91952ebc531247b7e543b285316aa7a6085
kiwi_j500 <mingureion@gmail.com>
2023-04-14 03:09:50 +0900

    주석수정

--- problems/boj10250_ACM호텔.py
+++ problems/boj10250_ACM호텔.py
@@ -5,7 +5,7 @@ t = int(input())
 for i in range(t):
     h, w, n = map(int,input().split())   # h, w, n
     a, b = math.ceil(n/h), n % h    #몫 -> ~호, 나머지-> 층 / 0~h까지는 1호니까 천장함수 사용
-    if b == 0:  # 몫이 0이면 0층이 아니라 최고층
+    if b == 0:  # 나머지가 0이면 0층이 아니라 최고층
         b = h
     print(str(b) +str(a).zfill(2))
 
