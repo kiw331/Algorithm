6ae4bf8e3ca2f4124bb2653112e80efcf5afa887
kiwi_j500 <mingureion@gmail.com>
2024-11-10 15:18:39 +0900

    boj1062 re

--- problems/boj/boj1062_가르침.py
+++ problems/boj/boj1062_가르침.py
@@ -5,10 +5,15 @@ words = [set(stdin.readline().rstrip()[4:-4])-{'a','n','t','i','c'} for _ in ran
 
 res =0
 if k>=5: # 배우는 글자가 5개 미만이면 어떤 단어도 읽지 못함
-    all = set() # (기본글자 제외한) 모든 글자 집합 
+    
+    
+    # (기본글자 제외한) 모든 글자 집합
+    all = set()  
     for w in words:
         all = all.union(w)
-    if len(all)<=k-5: # 모든 글자보다 배우는 글자가 더 많으면 모든 단어 읽을 수 있음
+        
+    # 모든 글자보다 배우는 글자가 더 많으면 모든 단어 읽을 수 있음
+    if len(all)<=k-5: 
         res = n
 
     else: # 읽을 수 있는 글자 조합 별 읽을 수 있는 단어 개수를 구함
--- problems/dup/boj1062_가르침.py
+++ problems/dup/boj1062_가르침.py
@@ -2,10 +2,8 @@
 import time
 s = time.time()  # 시작 시간 기록
 
-import random
-from sys import stdin
+from sys import stdin, exit
 from itertools import combinations as comb
-
 
 n, k = map(int, stdin.readline().split())
 words = []
@@ -14,28 +12,34 @@ for _ in range(n):
     w = set(w) - {'a','n','t','i','c'}   # antic 다섯 글자는 항상 배움
     words.append(w)
     
+
+
+
 res = 0
 if k >= 5:  #k가 5미만이면 배울 수 있는 단어는 없음.
     
+    
     # 모든 집합 합하기
     all = set().union(*words)
+    
+    if k-5 >= len(all):
+        res = n
 
     #가르칠 수 있는 문자 조합 lc (미리 기본 5문자 뺐으니까 조합 길이는 k-5)
     #lc = comb(all, k-5)
 
-
-    # 각 조합별 가르칠 수 있는 단어 개수 구하고 최대값이면 갱신
-    for c in lc:
-        tmp = 0
-        for w in words:
-            if w <= set(c):  #단어가 현 조합의 부분집합이다 = 읽을 수 있는 단어
-                tmp +=1
-        
-        if tmp > res:
-            res = tmp
+    else:
+        # 각 조합별 가르칠 수 있는 단어 개수 구하고 최대값이면 갱신
+        for c in comb(all, k-5):
+            tmp = 0
+            c= set(c)
+            for w in words:
+                if w <= c:  #단어가 현 조합의 부분집합이다 = 읽을 수 있는 단어
+                    tmp +=1
             
-        if tmp > n: #시간 초과 방지
-            break
+            if tmp > res:
+                res = tmp
+                
 
 print(res)
 
